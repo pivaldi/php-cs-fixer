e9db0576da25f8ebbae87cb2677ab8f694e14a61
pivaldi <git@ivaldi.me>
2024-08-15 00:49:27 +0200

    Improve config

--- php-cs-fixer-config.php
+++ php-cs-fixer-config.php
@@ -11,6 +11,13 @@ $config->setRules([
     'array_syntax' => ['syntax' => 'short'],
     'array_indentation' => true,
     'multiline_whitespace_before_semicolons' => true,
+    'method_argument_space' => ['ensure_fully_multiline' => true],
+    'declare_equal_normalize' => [
+        'space' => 'single',
+    ],
+    'binary_operator_spaces' => [
+        'default' => 'single_space'
+    ],
     'concat_space' => ['spacing' => 'one'],
 ]);
 
